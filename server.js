71da4c7d74a6c00acb13d26a977793a7cf2ff819
mskradde <skradde.marco@gmail.com>
2020-09-03 12:24:37 +0200

    Serve client

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const app = express();
+const path = require("path");
+
 const port = process.env.PORT || 3000;
 
-app.get("/", (request, response) => {
-  response.send("Hello World!");
-});
+// Serve any static files
+app.use(express.static(path.join(__dirname, "client/build")));
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
